bbed0a78336483fced2f818ebd33079848ab19d3
a <a@as-Virtual-Machine.local>
2026-07-14 02:01:01 -0700

    Integrate YAML configuration parsing into the application

--- app/Main.hs
+++ app/Main.hs
@@ -1,10 +1,23 @@
 module Main (main) where
 
+import Data.Yaml (FromJSON, decodeFileEither)
 import Options.Applicative (execParser, helper, strArgument)
 import Options.Applicative.Builder (info)
 import Relude
 
+data Config = Config
+  { benchmark :: Text,
+    theme :: Text
+  }
+  deriving (Generic)
+
+instance FromJSON Config
+
 main :: IO ()
 main = do
   file <- execParser $ info (strArgument mempty <**> helper) mempty
+  result <- decodeFileEither file
+  case result of
+    Left _ -> pure ()
+    Right (_ :: Config) -> pure ()
   pure ()
