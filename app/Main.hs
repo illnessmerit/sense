e77fecef86061f8b744badda3d40cb36b4aafe2c
a <a@as-Virtual-Machine.local>
2026-07-14 19:59:02 -0700

    Add support for reading wiktionary.tsv

--- app/Main.hs
+++ app/Main.hs
@@ -1,11 +1,29 @@
 module Main (main) where
 
+import Data.Csv (DecodeOptions (decDelimiter), FromNamedRecord, decodeByNameWith, defaultDecodeOptions, parseNamedRecord, (.:))
+import Data.Vector (Vector)
 import Data.Yaml (FromJSON, decodeFileEither)
 import Options.Applicative (execParser, helper, strArgument)
 import Options.Applicative.Builder (info)
 import Relude
 import System.Directory (createDirectoryIfMissing, getHomeDirectory)
 import System.FilePath ((</>))
+
+data Row = Row
+  { entry :: !Text,
+    prevalence :: !Double,
+    lemma :: !Bool
+  }
+  deriving (Show)
+
+instance FromNamedRecord Row where
+  parseNamedRecord r =
+    Row
+      <$> r
+      .: "entry"
+      <*> r
+      .: "prevalence"
+      <*> ((== ("true" :: Text)) <$> r .: "lemma")
 
 data Config = Config
   { benchmark :: Text,
@@ -20,6 +38,10 @@ main = do
   home <- getHomeDirectory
   key <- readFileBS $ home </> ".config/sense/key"
   createDirectoryIfMissing True $ home </> ".local/state/sense/"
+  content <- readFileLBS "wiktionary.tsv"
+  case decodeByNameWith (defaultDecodeOptions {decDelimiter = 9}) content of
+    Left _ -> pure ()
+    Right (_, rows :: Vector Row) -> pure ()
   file <- execParser $ info (strArgument mempty <**> helper) mempty
   result <- decodeFileEither file
   case result of
